8930c7ae80a243b48d532446cc1f0f863bed6f98
Christian Knappke <info@k5e.de>
2013-02-07 18:36:49 +0100

    Various changes to make the app presentable: + Added fluid layout - adapts to screen size and orientation + Changed progress bar: height and background + Progress bar changes color over time + Inactive buttons hide when clock is running

--- source/App.js
+++ source/App.js
@@ -5,10 +5,8 @@ enyo.kind({
 	fit: true,
 	components:[
 		{kind: "onyx.Toolbar", content: "Speaker's Clock"},
-//		{kind: "enyo.Scroller", fit: true, classes: "time-display", components: [
-			{name: "time", fit: true, classes: "time-display", allowHtml: true},
-//		]},
-		{kind: "onyx.ProgressBar", name: "progress", showStripes: false},
+		{name: "timeDisplay", fit: true, classes: "time-display", allowHtml: true, onresize:"displayResized"},
+		{kind: "onyx.ProgressBar", name: "progress", showStripes: false, classes: "app-progress-black", barClasses: "bar-color"},
 		{kind: "onyx.Toolbar", components: [
 			{kind: "onyx.Button", name: "buttonStart", content: "Start", ontap: "startClock"},
 			{kind: "onyx.Button", name: "buttonPlus", content: "+", ontap: "plusOne"},
@@ -24,30 +22,88 @@ enyo.kind({
 	theState: 0,
 	theTime: 0,
 	timeSet: 0,
-	timeMax: 5940, // 99min
+	timeMax: 5940, // 99 min.
+	timeDefault: 1200, //20 min.
+	timeOut: 1000,
 	timer: null,
+	timePercentGreen: 50,
+	timePercentYellow: 70,
+	timePercentRed: 90,
+	barStyle: null,
 	
 	create: function() {
 		this.inherited(arguments);
-		this.timeSet = 1200;
-		this.theTime = 1200;
+		this.barStyle = document.createElement('style');
+		this.barStyle.type = 'text/css';
+		this.barStyle.innerHTML = '.bar-color { background: grey; }';
+		this.timeSet = this.timeDefault;
+		this.theTime = this.timeDefault;
 		this.displayTime(this.theTime);
+		this.displayResized();
+	},
+	
+	displayResized: function (inSender, inEvent) {
+		var fontSize = 0;
+		var paddingTop =0;
+		var style = "";
+		var timeHeight = window.innerHeight - 176;
+		var timeWidth = window.innerWidth - 80;
+		fontSize = Math.floor((timeWidth) / 2.5);
+		if (fontSize > timeHeight)
+			fontSize = timeHeight;
+		paddingTop = Math.floor((timeHeight - fontSize) / 2 + 16);
+		this.$.timeDisplay.applyStyle("font-size", fontSize + "px");
+		this.$.timeDisplay.applyStyle("padding-top", paddingTop + "px");
+	},
+	
+	progressChanged: function () {
+		var color = this.changeColor (this.timeSet - this.theTime, this.timeSet, 
+			this.timePercentGreen, this.timePercentYellow, this.timePercentRed);
+		this.barStyle.innerHTML = ".app-bar-color { background-color: " + color +"; }";
+		this.$.progress.$.bar.applyStyle("background", color);
+	},
+	
+	changeColor: function (s, max, pg, py, pr) {
+		var gr = 0, gg = 255;
+		var yr = 255, yg = 255;
+		var rr = 255, rg = 0;
+		var color = "";
+		var ppg, ppy;
+		var r, g;
+		var dg = py - pg;
+		var dy = pr - py;
+		var p = s === 0 ? 0 : Math.floor((s / max * 100));
+		if (p <= pg) { // green
+			color = "rgb(0,255,0)";
+		} else if (p <= py) { //transition to yellow
+			ppg = p === pg ? 0 : ((p - pg)/ dg);
+			r = Math.floor(yr * ppg);
+			color = "rgb(" + r + ",255,0)" ;
+		} else if (p <= pr) { // transition to red
+			ppy = p === py ? 0 : ((p - py) / dg);
+			g = Math.floor(yg * (1 - ppy));
+			color = "rgb(255,"+ g +",00)";
+		} else { // red
+			color = "rgb(255,0,0)";
+		}
+		return color;
 	},
 	
 	startClock: function() {
+		if(this.theTime === 0) 
+			return;
 		switch(this.theState) {
 		case this.states.stopped:
-			this.theState = this.states.running;
-			this.$.buttonStart.setContent("Pause");
-			this.timer = setTimeout(this.updateTime.bind(this), 1000);
-			break;
+		//fallthrough
 		case this.states.paused:
 			this.theState = this.states.running;
 			this.$.buttonStart.setContent("Pause");
-			this.timer = setTimeout(this.updateTime.bind(this), 1000);
+			this.buttonsOff();
+			this.timer = setTimeout(this.updateTime.bind(this), this.timeOut);
 			break;
 		case this.states.running:
 			this.theState = this.states.paused;
+			this.buttonsOn();
 			this.$.buttonStart.setContent("Start");
 			break;
 		}
@@ -58,29 +114,37 @@ enyo.kind({
 			return;
 		this.theTime--;
 		if(this.theTime < 0)
-			this.theTime === 0
+			this.theTime = 0;
 		this.displayTime(this.theTime);
 		if (this.theTime > 0 && this.theState === this.states.running)
-			setTimeout(this.updateTime.bind(this), 1000);
+			setTimeout(this.updateTime.bind(this), this.timeOut);
 		else if (this.theTime === 0)
 			this.stopTime();
+		this.progressChanged();
 	},
 	
 	stopTime: function() {
 		this.theState = this.states.stopped;
+		this.buttonsOn();
 		this.$.buttonStart.setContent("Start");
 		
 	},
 	
 	buttonsOff: function() {
+			this.$.buttonPlus.hide();
+			this.$.buttonMinus.hide();
+			this.$.buttonReset.hide();
 	},
 	
 	buttonsOn: function() {
+			this.$.buttonPlus.show();
+			this.$.buttonMinus.show();
+			this.$.buttonReset.show();
 	},
 	
 	plusOne: function(inSender, inEvent) {
 		if(this.theState !== this.states.running) {
-			this.theTime /= 60;
+			this.theTime = Math.floor(this.theTime / 60);
 			this.theTime += 1;
 			this.theTime *= 60;
 			if(this.theTime > this.timeMax) {
@@ -93,7 +157,8 @@ enyo.kind({
 	
 	minusOne: function(inSender, inEvent) {
 		if(this.theState !== this.states.running) {
-			this.theTime /= 60;
+			var plus1 = Math.floor(this.theTime % 60) > 29 ? 1 : 0;
+			this.theTime = Math.floor(this.theTime / 60) + plus1;
 			this.theTime -= 1;
 			this.theTime *= 60;
 			if(this.theTime < 0) {
@@ -118,15 +183,8 @@ enyo.kind({
 		var p;
 		if(m < 10) m = "0"+m;
 		if(s < 10) s = "0"+s;
-		this.$.time.setContent(m + ":" + s);
+		this.$.timeDisplay.setContent(m + ":" + s);
 		p = 100 * (this.timeSet - t) / this.timeSet;
-		if(p < 70) { 
-			this.$.progress.barClasses += "background: green;";
-		} else if(p < 90) {
-			this.$.progress.barClasses = ".bar-yellow";
-		} else {
-			this.$.progress.barClasses = ".bar-red";
-		}
 		this.$.progress.animateProgressTo(p);
 	}
 });
